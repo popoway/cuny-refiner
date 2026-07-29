21e0e4bcd07cbf734a92ba18beae0a6a5a178611
Ming Lei <31576265+popoway@users.noreply.github.com>
2026-07-29 16:04:32 -0400

    feat: firefox support

--- welcome.js
+++ welcome.js
@@ -1,8 +1,15 @@
 const openExtensionsBtn = document.getElementById('open-extensions');
 const closeTabBtn = document.getElementById('close-tab');
+const isFirefox = navigator.userAgent.includes('Firefox');
+
+if (openExtensionsBtn && isFirefox) {
+  openExtensionsBtn.hidden = true;
+}
 
 function openExtensionsMenu() {
-  const detailsUrl = `chrome://extensions/?id=${chrome.runtime.id}`;
+  const detailsUrl = isFirefox
+    ? 'about:addons'
+    : `chrome://extensions/?id=${chrome.runtime.id}`;
   chrome.tabs.create({ url: detailsUrl });
 }
 
